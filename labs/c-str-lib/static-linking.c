139a85343e9324a6a1a57403aec58e50a310aa0e
Oscar Fernández Moreno <oscar.gohan2010@hotmail.com>
2021-09-30 23:21:22 +0000

    feat: str lib almost done

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char*);
+
+char* mystradd(char*, char*);
+
+int mystrfind(char*, char*);
+
+
+
+int main(int argc, char **argv) {
+
+    if(argc != 4){
+        printf("Program just support the following arguments: \n");
+        printf("./main.o -add \"Initial String\" \"This is the rest to be added\" \n");
+        printf("./main.o -find \"Initial String " "Init\" \n");
+        return -1;
+    }
+
+    char *oper = argv[1];
+
+    if(strcmp("-add", oper) != 0 && strcmp("-find", oper)!= 0){
+        printf("Program just support the following arguments: \n");
+        printf("./main.o -add \"Initial String\" \"This is the rest to be added\" \n");
+        printf("./main.o -find \"Initial String \" \"Init\" \n");
+        return -1;
+    }
+
+    char *fstring = argv[2];
+    char *sstring = argv[3];
+
+     
+    if(strcmp("-add", oper) == 0 ){
+        printf("Initial Length  : %i \n", mystrlen(fstring));
+       char *myNewString = mystradd(fstring, sstring); 
+        printf("New String      : %s \n", myNewString);
+        printf("New Length      : %i \n", mystrlen(myNewString));
+        return 0;
+    }
+
+    int position = -1;
+    if(strcmp("-find", oper) == 0){
+        position = mystrfind(fstring, sstring);
+        if(position != -1){
+            printf("['%s'] string was found at [%i] position \n", sstring, position);
+        }else{
+            printf("['%s'] string was not found in %s \n", sstring, fstring);
+        }
+    }
+
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,58 @@
 
-
 int mystrlen(char *str){
-    return 0;
+    int count = 0;
+    while(*str != '\0')
+    {
+        count++;
+        *str++;
+    }
+    return count;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *result = malloc(mystrlen(origin) + mystrlen(origin) + 1);
+    strcpy(result, origin);
+    strcat(result, addition);
+    return result;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    
+    if(mystrlen(origin) < mystrlen(substr) || mystrlen(substr) == 0 || mystrlen(origin) == 0){
+        return -1;
+    }
+
+    int size_origin = mystrlen(origin);
+    int size_substring = mystrlen(substr);
+    int size_find_substring = 0;
+    int position_substring = size_origin+1;
+    int iter_sub = 0;
+    int iter_ori = 0;
+    char o = "";
+    char s = substr[0];
+    for(int i = 0; i < size_origin; i++){
+        o = origin[i];
+        if(o == s){
+            position_substring = i;
+            iter_ori = i;
+            iter_sub = 0;
+            while(iter_ori < size_origin && iter_sub < size_substring && o == s){
+                size_find_substring ++;
+                iter_sub++;
+                iter_ori++;
+                s = substr[iter_sub];
+                o = origin[iter_ori];
+            }
+            if(size_find_substring == size_substring){
+                return position_substring;
+            }
+            position_substring = -1;
+            size_find_substring = 0;
+            i = iter_ori;
+            s = substr[0];
+        }
+    }
+
+    return -1; 
+    
 }
