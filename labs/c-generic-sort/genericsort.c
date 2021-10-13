45eba1a6c3251df185a05d563816d3eb8f8b8b1a
Oscar Fernández Moreno <oscar.gohan2010@hotmail.com>
2021-10-13 18:21:52 +0000

    Generic Sort Before Make Test

--- labs/c-generic-sort/genericsort.c
+++ labs/c-generic-sort/genericsort.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include<stdlib.h>
 
 void quicksort(void *lineptr[], int left, int right,
 	   int (*comp)(void *, void *));
@@ -6,7 +10,139 @@ void quicksort(void *lineptr[], int left, int right,
 void mergesort(void *lineptr[], int left, int right,
 	   int (*comp)(void *, void *));
 
+int numcmp(const void *p1, const void *p2)
+{
+     double v1 = atof(p1),
+        v2 = atof(p2);
+
+    return (int) (v1 - v2);
+}
+
 int main(int argc, char **argv)
 {
+	if(argc>6 || argc < 3){
+		printf("Program should have the correct arguments \n");
+		printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+		printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+		return -1;
+	}
+
+	bool isNumberFile = true;
+	char *fileName = 'fileName';
+	char *sortType = 'mergesort';
+	char *nameOutputFile = 'sorted.txt';
+	
+	if(strcmp("-n", argv[1]) != 0){
+		isNumberFile = false;
+		fileName = argv[1];
+		sortType = argv[2];
+		if(strcmp("-o", argv[3]) == 0){
+			nameOutputFile = argv[4];
+		}else{
+			printf("Program should have the correct arguments (output file) \n");
+			printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+			printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+			return -1;
+		}
+	}else{
+		fileName = argv[2];
+		sortType = argv[3];
+		if(strcmp("-o", argv[4]) == 0){
+			nameOutputFile = argv[5];
+		}else{
+			printf("Program should have the correct arguments (output file) \n");
+			printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+			printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+			return -1;
+		}
+	}
+
+
+	//Read document
+	// Declaration pointer
+	FILE *fptr;
+	char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+	int sizeFile = 0;
+	ssize_t maxLineSize =0;
+
+	fptr = fopen(fileName,"r");
+
+	//Check if the file exist
+	if (fptr  == NULL){
+		printf("Program should have the correct arguments and make sure the document is on the same directory \n");
+		printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+		printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+		return -1;
+	}
+
+	//Get the size of the file
+	while ((read = getline(&line, &len, fptr)) != -1) {
+		if(strcmp("\n", line) != 0 && strcmp(" ", line) != 0){
+			  //printf("Retrieved line of length %zu:\n", read);
+			  if(read > maxLineSize){
+				  maxLineSize = read;
+			  }
+        	//printf("%s", line);
+			sizeFile++;
+			//printf("Document Size of: %i \n", sizeFile);
+		}
+    }
+
+	fptr = fopen(fileName,"r");
+	int left = 0;
+	int right = sizeFile - 1;
+	int numLines = 0;
+	void *lineptr[sizeFile];
+	len = 0;
+	while ((read = getline(&line, &len, fptr) != -1)) {
+		if(strcmp("\n", line) != 0 && strcmp(" ", line) != 0){
+			lineptr[numLines] = malloc(strlen(line));
+			strcpy(lineptr[numLines++], line);
+		}
+		
+    }
+	
+	
+	// Save the element depending of the -n command
+	if(isNumberFile){
+		if(strcmp("-quicksort", sortType) == 0){
+			quicksort(lineptr, left, right, (int (*)(void *, void *)) numcmp);
+		}else if(strcmp("-mergesort", sortType) == 0){
+			mergesort(lineptr, left, right, (int (*)(void *, void *))numcmp);
+		}else {
+			printf("Program should have the correct arguments and make sure the sorted array \n");
+			printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+			printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+			return -1;
+		}
+	}else{
+		if(strcmp("-quicksort", sortType) == 0){
+			quicksort( lineptr, left, right, (int (*)(void *, void *))strcmp);
+			
+		}else if(strcmp("-mergesort", sortType) == 0){
+			mergesort( lineptr, left, right, (int (*)(void *, void *))strcmp);
+			
+		}else {
+			printf("Program should have the correct arguments and make sure the sorted array \n");
+			printf ("$ ./genericsort -n numbers.txt -quicksort -o qs_sorted_numbers.txt \n");
+			printf ("$ ./genericsort strings.txt -mergesort -o ms_sorted_strings.txt \n");
+			return -1;
+		}
+	} 
+
+	/* for (int i = 0; i < sizeFile; i++) {
+		printf( "%s", (char *)lineptr[i]);
+	}  */
+
+	FILE *output;
+	output = fopen(nameOutputFile, "w+");
+	for (int i = 0; i < sizeFile; i++) {
+        fprintf(output, "%s", (char *)lineptr[i]);
+    }
+
+	fclose(fptr);
+	fclose(output);
     return 0;
 }
--- labs/c-generic-sort/mergesort.c
+++ labs/c-generic-sort/mergesort.c
@@ -1,4 +1,53 @@
 
-void mergesort(void *lineptr[], int left, int right,
-	       int (*comp)(void *, void *)) {
+void mergesort(void *lineptr[], int left, int right, int (*comp)(void *, void *)) {
+
+	if(left < right){
+		int middle = left + (right - left) / 2;
+		mergesort(lineptr, left, middle, comp);
+		mergesort(lineptr, middle + 1, right, comp);
+		merge(lineptr, left ,middle, right, comp);
+	}
 }
+
+
+void merge(void *lineptr[], int left, int middle, int right, int (*comp)(void *, void *)) {
+	int n1 = middle - left + 1;
+	int n2 = right - middle;
+
+	void *L[n1];
+	void *M[n2];
+
+	for (int i = 0; i < n1; i++)
+    	L[i] = lineptr[left + i];
+  	for (int j = 0; j < n2; j++)
+    	M[j] = lineptr[middle + 1 + j];
+
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = left;
+
+	while (i < n1 && j < n2) {
+		if ((*comp)(L[i], M[j]) < 0) {
+			lineptr[k] = L[i];
+			i++;
+		} else {
+			lineptr[k] = M[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		lineptr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		lineptr[k] = M[j];
+		j++;
+		k++;
+	}	
+}
+
